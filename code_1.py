98e83acdacd27e25de683843024e15215b409af5
GurkanKSezer <gurkankerim.sezer@std.yeditepe.edu.tr>
2026-04-10 21:32:02 +0300

    Update code_1.py

--- code_1.py
+++ code_1.py
@@ -2,16 +2,7 @@ import time
 
 # First line of your first code
 print("Hello world!")
-print("New line is added")
 
 # New line is added
 print("Hey! I'm changing the file!")
 
-for i in range(5):
-    print("Hey there! Wassup?")
-
-for i in range(5,-1,-1):
-    print("--->", i)
-    time.sleep(1)
-
-print("Rocket was launched!")
