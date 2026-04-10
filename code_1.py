db91cdd6eb1d5c7d7a797b3bbcca0d19f103165b
Gurkan Kerim Sezer <gurkankerim.sezer@std.yeditepe.edu.tr>
2026-04-10 21:21:53 +0300

    code file was loaded

--- code_1.py
+++ code_1.py
@@ -6,3 +6,18 @@ print("Hello world!")
 # New line is added
 print("Hey! I'm changing the file!")
 
+<<<<<<< HEAD
+=======
+for i in range(5):
+    print("Hey there! Wassup?")
+
+for i in range(5,-1,-1):
+    print("--->", i)
+    time.sleep(1)
+
+print("Rocket was launched!")
+
+
+# First line of your first code
+print("Hello world!")
+>>>>>>> dc7f526 (code file was loaded)
