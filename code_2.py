652ecc2cabfe8f968ed1375049dde07340644547
Gurkan Kerim Sezer <gurkankerim.sezer@std.yeditepe.edu.tr>
2026-04-10 21:49:31 +0300

    second code file was loaded

--- code_2.py
+++ code_2.py
@@ -7,11 +7,11 @@ current_date_and_time = datetime.now()
 time_now = datetime.now()
 current_time = time_now.strftime("%H:%M:%S")
 # time_now = datetime.now().strftime("%H:%M:%S")
-
+	
 current_time_2 = time.ctime()
-
+	
 current_time_3 = time.strftime("%H:%M:%S")
-
+	
 print("The current date and time is", current_date_and_time)
 print(f"\nThe current time is {current_time}\n")
 print(current_time_2)
